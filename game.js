d04f0580b01109a700a811aff25d7cfb765eeab6
Taizu296 <noir.nails777@gmail.com>
2026-09-23 12:24:33 +0200

    game.js aktualisieren

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
-// Shadow Hunters
+const player = document.getElementById("player");
+
+let x = window.innerWidth / 2;
+let y = window.innerHeight / 2;
+
+function movePlayer(event) {
+  const touch = event.touches[0];
+
+  x = touch.clientX;
+  y = touch.clientY;
+
+  player.style.left = x + "px";
+  player.style.top = y + "px";
+}
+
+document.addEventListener("touchmove", movePlayer);
+document.addEventListener("touchstart", movePlayer);
